9ce2e323ddd13381e1339dade996b78a286e5bd7
Siddhant Tohan <siddhant.tohan@gmail.com>
2021-11-03 19:06:08 +0530

    Successfully created the popup form

--- script.js
+++ script.js
@@ -2,10 +2,14 @@
 const bookCase = document.querySelector('.book-case');
 const form = document.querySelector('.input-form');
 const newBook = document.querySelector('.new-book');
-const popUpDiv = document.querySelector('.pop-up')
+const popUpDiv = document.querySelector('.pop-up');
+const closeButton = document.querySelector('.close-form');
+const display = document.querySelector('.display');
+const heading = document.querySelector('header');
 
 // Global Variables
 let books = [];
+let showing = false;
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -14,16 +18,16 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
-function addBook(newBook){
+function addBook(newBook) {
   books.push(newBook);
 }
 
-function displayBooks(){
+function displayBooks() {
   // using of loop to access array easily
   bookCase.innerHTML = '';
   // clearing any previous outputs
 
-  for(const i of books){
+  for (const i of books) {
     let insertBook = document.createElement('div');
     insertBook.classList.add('book-card')
 
@@ -31,33 +35,43 @@ function displayBooks(){
     cardTitle.classList.add('card-title');
     cardTitle.textContent = i.title;
     insertBook.appendChild(cardTitle)
-    
+
     let cardAuthor = document.createElement('div');
     cardAuthor.classList.add('card-author');
     cardAuthor.textContent = i.author;
     insertBook.appendChild(cardAuthor)
-    
+
     let cardPages = document.createElement('div');
     cardPages.classList.add('card-pages');
     cardPages.textContent = i.pages;
     insertBook.appendChild(cardPages);
-    
+
     bookCase.appendChild(insertBook);
   }
 }
 
 
-function openPopUp(e){
-  console.log(e);
+function openPopUp(e) {
   popUpDiv.classList.toggle('hidden');
+  form.classList.toggle('hidden');
+  if (showing === false) {
+    showing = true;
+    display.setAttribute('style', "filter: opacity(0.5)");
+    heading.setAttribute('style', "filter: opacity(0.5)")
+  } else {
+    display.removeAttribute('style');
+    heading.removeAttribute('style');
+    showing = false;
+  }
+
 }
 
-function fetchValues(e){
+function fetchValues(e) {
   const title = form.elements['title'].value;
   const author = form.elements['author'].value;
   const pages = form.elements['pages'].value;
   const read = form.elements['read'].value;
-  const newBook = new Book(title,author,pages,read);
+  const newBook = new Book(title, author, pages, read);
   addBook(newBook);
   displayBooks();
   openPopUp();
@@ -66,5 +80,9 @@ function fetchValues(e){
 
 form.addEventListener('submit', fetchValues);
 newBook.addEventListener('click', openPopUp);
-
-
+closeButton.addEventListener('click', openPopUp);
+window.addEventListener('click', (e) => {
+  if(e.target.classList.contains('pop-up')){
+    openPopUp();
+  }
+})
